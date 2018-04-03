ad0be5b05f54bbed306f1fffb761a4ee9cb9ac0f
jkaupp <jkaupp@gmail.com>
2018-04-03 16:22:43 -0400

    a little polish

--- R/plot.R
+++ R/plot.R
@@ -26,21 +26,19 @@ labels <- plot_data %>%
   group_by(state) %>%
   filter(all(avg_tuition > nat_avg)) %>%
   pull(state) %>%
-  unique()
-
+  uniqe()
 
 plot <- plot_data %>%
   ggplot(., aes(x = year, y = avg_tuition, group = state)) +
+  geom_text_repel(data = filter(plot_data, state %in% labels, year == "2015-16"), aes(label = state), direction = "y", nudge_x = 0.1, segment.size = 0.1, hjust = 0, family = "Oxygen", size = 3) +
   geom_path(color = "grey50", size = 0.5, alpha = 0.5) +
   geom_point(color = "grey50") +
-  geom_text_repel(data = filter(plot_data, state %in% labels, year == "2015-16"), aes(label = state), direction = "y", nudge_x = 0.1, segment.size = 0.1, hjust = 0) +
   geom_path(data = nat_avg, color = "red", size = 1) +
   geom_point(data = nat_avg, color = "red") +
-  geom_text_repel(data = filter(plot_data, state == "National Average", year == "2015-16"), aes(label = state), direction = "y", nudge_x = 0.1, segment.size = 0.1, hjust = 0, color = "red", segment.colour = "black", fontface = "bold") +
   scale_y_continuous(labels = scales::dollar) +
-  labs(x = NULL, y = NULL, title = "Comparison of the average US tuition growth between 2005 and 2015", subtitle = "Eastern and Northeastern students consistently face tutition above the national average.", caption = "\nData: http://trends.collegeboard.org/ | Graphic: @jakekaupp") +
-  theme_minimal() +
+  labs(x = NULL, y = NULL, title = "Comparison of the average US tuition growth between 2005 and 2015", subtitle = "Eastern and Northeastern students consistently face tutition above the national average, indicated by the red line.", caption = "\nData: http://trends.collegeboard.org/ | Graphic: @jakekaupp") +
+  theme_minimal(base_family = "Oswald-Light") +
   theme(panel.grid.minor = element_blank())
 
-ggsave(plot, filename = glue('tidyweek-{Sys.Date()}.png'), height = 4, width = 4, dpi = 300)
+ggsave(plot, filename = glue('tidyweek-{Sys.Date()}.png'), height = 8, width = 6, dpi = 300)
 
