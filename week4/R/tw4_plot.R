3560dab9cbaaf7a385b1c94c5839a304eb10d3f0
Jake Kaupp <jkaupp@gmail.com>
2018-04-25 14:37:38 +0000

    added wk4 waffle

--- week4/R/tw4_plot.R
+++ week4/R/tw4_plot.R
@@ -6,18 +6,18 @@ library(ggiraph)
 library(glue)
 library(waffle)
 
-make_tooltip <- function(occupation, female, male, salary_gap, ...) {
+make_tooltip <- function(occupation, female, male, income_gap, ...) {
  
   glue('<div class="tipchart">
       <h3>{occupation}</h3>
-      <h4>Men make {ifelse(salary_gap <= 1, percent(1-round(salary_gap, 2)), percent(round(salary_gap, 2)))} {ifelse(salary_gap <= 1, "less", "more")} than women</h4>
+      <h4>Mens taxable income {ifelse(income_gap <= 1, percent(1-round(income_gap, 2)), percent(round(income_gap, 2)))} {ifelse(income_gap <= 1, "less", "more")} than womens</h4>
       <table>
       <tr class="tiprow">
-      <td class="tipheader">Average Male Salary:</td>
+      <td class="tipheader">Average Male Taxable Income:</td>
       <td class="tiptext">{dollar(male)}</td>
       </tr>
       <tr class="tiprow">
-      <td class="tipheader">Average Female Salary:</td>
+      <td class="tipheader">Average Female Taxable Income:</td>
       <td class="tiptext">{dollar(female)}</td>
       </tr>
       </table>
@@ -27,22 +27,22 @@ make_tooltip <- function(occupation, female, male, salary_gap, ...) {
 
 
 # Read in the data
-salary_data <- dir(here("week4","data"), pattern = "salary", full.names = TRUE) %>% 
+income_data <- dir(here("week4","data"), pattern = "salary", full.names = TRUE) %>% 
   read_csv(locale = locale("en"))
  
 # Clean occupation up a bit.  Some rouge unicodes in there.
-tidy_gap <- salary_data %>% 
+tidy_gap <- income_data %>% 
   mutate(occupation = iconv(occupation, "UTF-8", "UTF-8",sub='')) %>% 
   spread(gender, average_taxable_income) %>%
   set_names(tolower(names(.))) %>% 
   group_by(occupation) %>% 
   summarize_at(c("female", "male"), sum, na.rm = TRUE) %>% 
   filter(female != 0, male != 0) %>% 
-  mutate(salary_gap = male/female)
+  mutate(income_gap = male/female)
 
 plot_data <- tidy_gap %>% 
- mutate(fill = if_else(salary_gap >= 1, "grey80", "#ffd700"),
-         alpha = if_else(salary_gap >= 1, 0.2, 1)) %>% 
+ mutate(fill = if_else(income_gap >= 1, "grey80", "#ffd700"),
+         alpha = if_else(income_gap >= 1, 0.2, 1)) %>% 
   mutate(tooltip = pmap(., make_tooltip)) %>% 
   mutate(tooltip = gsub("\\\n", "", tooltip)) %>% 
   mutate(tooltip = gsub("'", "", tooltip)) %>% 
@@ -57,32 +57,31 @@ plot <- ggplot(plot_data, aes(x = female, y = male, fill = fill)) +
   scale_x_continuous(labels = dollar, limits = c(0, 600000)) +
   scale_fill_identity() +
   scale_alpha_identity() +
-  labs(x = "Average Female Salary", 
-       y = "Average Male Salary", 
-       title = "Gender Pay Equality in Australia",
-       subtitle = str_wrap("Average male salary plotted against average female salary by occupation. Yellow dots indicate occupations where women make as much or more than their male counterparts, 
-       line indicates pay equality. Hover over points for occupation, pay disparity and salary information.", 100),
+  labs(x = NULL, 
+       y = NULL, 
+       title = "Gender Differences in Taxble Income in Australia",
+       subtitle = str_wrap("Average male taxable income plotted against average female taxable income by occupation. Yellow dots indicate occupations where women have more taxable income than their male counterparts, 
+       line indicates income equality. Hover over points for occupation, % difference and detailed income.", 100),
        caption = "Data: data.gov.au | Graphic: @jakekaupp") +
   theme_jk()
 
-ggiraph(ggobj = plot, width_svg = 8, width = 1, tooltip_extra_css = tooltip_css)
+ggiraph(ggobj = plot, width_svg = 9, width = 1, tooltip_extra_css = tooltip_css)
 
 waffle_data <- tidy_gap %>% 
   ungroup() %>% 
-  mutate(category = case_when(salary_gap > 1 ~ "Men earn more",
-                              salary_gap < 1 ~ "Women earn more"))%>% 
+  mutate(category = case_when(income_gap > 1 ~ "Men have more income",
+                              income_gap < 1 ~ "Women have more income"))%>% 
   count(category) %>% 
   pull(n) %>% 
-  set_names(., c("Men Earn more", "Women Earn More"))
+  set_names(., c("Men have more income", "Women have more income"))
 
 waffle(waffle_data, 
        rows = 14,
        size = 1,
        colors = c("dodgerblue3", "deeppink"), 
        legend_pos = "bottom", 
-       title = "Out of 1092 occupations on record, men earn more than women in 1011 of them.  That's 92.5% of jobs for those counting at home.") + 
+       title = "Out of 1092 occupations on record, men have more taxable income than women in 1011 of them.  That's 92.5% of occupations for those counting at home.") + 
   theme_jk() +
   labs(caption = "Data: data.gov.au | Graphic: @jakekaupp") +
   theme(axis.text = element_blank(),
         legend.position = "bottom")
-
