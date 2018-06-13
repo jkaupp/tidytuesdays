e87c1db902221ce8a4f74e4cf5ce9bf466d7ae32
Jake Kaupp <jkaupp@gmail.com>
2018-06-13 17:51:51 +0000

    Finished week 11, updated fonts

--- week11/R/tw11_plot.R
+++ week11/R/tw11_plot.R
@@ -10,6 +10,7 @@ raw_data <- read_csv("https://github.com/rfordatascience/tidytuesday/raw/master/
   select(-X1)
 
 font_add_google("Oswald","Oswald-Light", regular.wt = 300)
+font_add_google("Scope One","Scope One")
 
 vplayout <- function(x, y) viewport(width=11/3, height=8.5, layout.pos.row = x, layout.pos.col = y)
 
@@ -33,12 +34,7 @@ build_treemap <- function(x, y, size)  {
           border.col = "white",
           border.lwds = 1,
           position.legend = "none",
-          palette = c("#ffd700",
-          "#fa8775",
-          "#ea5f94",
-          "#cd34b5",
-          "#9d02d7",
-          "#0000ff"),
+          palette = nord("baie_mouton"),
           align.labels = list(c("left","top"), c("right","bottom")),
           drop.unused.levels = TRUE,
           vp = vplayout(x,y))
@@ -49,12 +45,14 @@ build_treemap <- function(x, y, size)  {
 
 fifa_maps <- function() {
   grid.newpage()
-  pushViewport(viewport(layout = grid.layout(1, 3)))
+  pushViewport(viewport(layout = grid.layout(3, 3, heights = c(0.1, 0.8, 0.1))))
   par(mai=c(0,0,0,0))
   
-  build_treemap(1, 1, "population_share")
-  build_treemap(1, 2, "tv_audience_share")
-  build_treemap(1, 3, "gdp_weighted_share")
+  grid.text("Treemaps Comparing FIFA Share Differences by Confederation and Country", x = 0.1, hjust = 0, vp = vplayout(1,1), gp = gpar(fontfamily = "Oswald-Light", fontsize = 30))
+  build_treemap(2, 1, "population_share")
+  build_treemap(2, 2, "tv_audience_share")
+  build_treemap(2, 3, "gdp_weighted_share")
+  grid.text("Data: fivethirtyeight.com | Graphic: @jakekaupp", x = 0.5, vp = vplayout(3,3), gp = gpar(fontfamily = "Scope One", fontsize = 10))
   
 }
 
