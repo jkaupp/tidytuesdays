558bc2140434bbdb90e4403924890a96761d0a15
Jake Kaupp <jake.kaupp@queensu.ca>
2019-04-12 13:29:14 -0400

    Finshed week 15

--- 2019/week14/R/analysis.R
+++ 2019/week14/R/analysis.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(lubridate)
 library(jkmisc)
+library(here)
 library(patchwork)
 
 bike_traffic <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-02/bike_traffic.csv")
@@ -20,7 +21,7 @@ glyph <- ggplot(by_year, aes(x = crossing, y = total_bikes, group = year)) +
   geom_point(size = 1, aes(color = crossing)) +
   facet_grid(year ~ month, labeller = labeller(.cols = set_names(month.abb, 1:12)), switch = "y") +
   scale_color_manual(values = tol7qualitative) +
-  theme_jk(grid = FALSE) +
+  theme_jk(grid = FALSE, plot_title_size = 14) +
   labs(x = NULL, 
        y = NULL) +
   theme(axis.text = element_blank(),
@@ -33,14 +34,15 @@ main <- by_year %>%
   geom_path() +
   geom_point(size = 3, aes(color = crossing)) +
   scale_color_manual("Crossing", values = tol7qualitative) +
-  theme_jk(grid = FALSE) +
+  theme_jk(grid = FALSE, plot_title_size = 14) +
    labs(x = NULL, 
        y = NULL) +
   theme(axis.text = element_blank(),
         strip.text.y = element_text(angle = 180))
 
-wrap_plots(list(main, glyph), widths = c(0.25, 0.75)) +
+plot <- wrap_plots(list(main, glyph), widths = c(0.25, 0.75)) +
   plot_annotation(title = "Annual Patterns in Seatle Bicycle Traffic", 
-                  subtitle = str_wrap("This chart is a parallel coordinates plot that illustrates the monthly bike traffic at Seattle crossings.  A  colored dot represents each crossing and vertical position represents the total number of riders counted each month.  You can observe the year over year trends, as well as see which crossings experience cyclical patterns and which remain stable.", 170),
+                  subtitle = str_wrap("This chart is glyph plot using multiple parallel coordinates plots to illustrate the monthly bike traffic at Seattle crossings.  A  colored dot represents each crossing and vertical position represents the total number of riders counted each month.  You can observe the year over year trends, as well as see which crossings experience cyclical patterns and which remain stable.", 155),
                   theme = theme_jk())
 
+ggsave(here("2019", "week14", "tw14_plot.png"), plot, width = 12, height = 6)
---
+++ 2019/week15/R/analysis.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(lubridate)
+library(ggbeeswarm)
+library(here)
+library(jkmisc)
+library(nord)
+
+player_dob <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-09/player_dob.csv")
+
+grand_slams <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-09/grand_slams.csv")
+
+grand_slam_timeline <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-09/grand_slam_timeline.csv")
+
+
+plot_data <- player_dob %>% 
+  select(name, date_of_birth) %>% 
+  left_join(grand_slams, by = "name") %>% 
+  mutate(age = interval(date_of_birth, tournament_date)/years(1)) %>% 
+  group_by(name) %>% 
+  filter(n()>1)
+  
+
+order <- plot_data %>% 
+  group_by(name) %>% 
+  filter(rolling_win_count == max(rolling_win_count)) %>% 
+  arrange(rolling_win_count) %>% 
+  pull(name)
+  
+
+plot <- ggplot(plot_data, aes(x = age, y = factor(name, order), size = rolling_win_count, color = gender, alpha = rolling_win_count)) +
+  geom_point(aes(group = name)) + 
+  facet_wrap(~gender, scales = "free_y") +
+  scale_color_manual(values = c("#C01E65","#117AB3")) +
+  scale_size_area("Rolling Win Count") +
+  guides(size = guide_legend(override.aes = list(shape = 21, color = "black")), color = FALSE, alpha = FALSE) +
+  theme_jk(grid = "XY") +
+  theme(legend.position = "bottom") +
+  labs(x = NULL,
+       y = NULL,
+       title = "Bright Stardom or Fading Obscurity: Looking at Players Major Wins Across their Careers.",
+       subtitle = str_wrap("The chart plots cummulative major wins against player age. The size and transparency of each point is mapped to the cummulative number of majors won at each age.  Looking at the data we can see the hot streaks in certain players, as well as the dominance of certain champions.", 110),
+       caption = "Data: wikipedia | Graphic: @jakekaupp")
+  
+ggsave(here("2019","week15","tw15_plot.png"), type = "cairo", width = 10, height = 12)
