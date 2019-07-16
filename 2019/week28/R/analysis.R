ec7b65b3ec704b9338414fc5006148def9e23c44
Jake Kaupp <jake.kaupp@queensu.ca>
2019-07-15 21:16:02 -0400

    incorporated feedback per Xan Gregg

--- 2019/week28/R/analysis.R
+++ 2019/week28/R/analysis.R
@@ -14,7 +14,8 @@ data <- squads %>%
   mutate(pos = case_when(pos == "DF" ~ "Defense",
                          pos == "FW" ~ "Forward",
                          pos == "MF" ~ "Mid-Field")) %>% 
-  mutate(desc = glue("{club}\n{caps} Matches, {goals} Goals"))
+  mutate(desc = glue("{club}\n{caps} Matches, {goals} Goals")) %>% 
+  mutate(pos = factor(pos, c("Defense", "Mid-Field", "Forward")))
 
 means <- data %>% 
   group_by(pos) %>% 
@@ -22,17 +23,17 @@ means <- data %>%
 
 plot <- ggplot(data, aes(x = age, y = idx)) +
   geom_point(color = "grey20") +
-  geom_mark_circle(aes(label = player, description = desc, filter = player == "Khadija Shaw")) +
-  geom_mark_circle(aes(label = player, description = desc, filter = player  == "Lea Schüller")) +
-  geom_mark_circle(aes(label = player, description = desc, filter = player  == "Ainon Phancha")) +
+  geom_mark_circle(aes(label = player, description = desc, filter = player == "Khadija Shaw"), expand = unit(4, "mm")) +
+  geom_mark_circle(aes(label = player, description = desc, filter = player  == "Lea Schüller"), expand = unit(4, "mm")) +
+  geom_mark_circle(aes(label = player, description = desc, filter = player  == "Ainon Phancha"), expand = unit(4, "mm")) +
   geom_hline(data = means, aes(yintercept = idx), color = "firebrick") +
   theme_jk() +
   facet_wrap(~pos, nrow = 1) +
   labs(y = NULL,
        x = "Age",
        title = "Efficient Scorers Competing in the Womens World Cup by Position and Age",
-       subtitle = str_wrap("Goals per games played in international play by player age.  The highly efficient players at each position are a mix of newcomers and seasoned veterans, illustrating consistency in some players through their career.", 150),
+       subtitle = str_wrap("Goals per games played in international play by player age.  The highly efficient players at each position are a mix of newcomers and seasoned veterans, illustrating consistency in some players through their career.", 120),
        caption = "Data: data.world | Graphic : @jakekaupp")
 
-ggsave("")
+ggsave(here("2019", "week28", "tw28_plot.png"), plot = plot, width = 10, height = 6)
 
