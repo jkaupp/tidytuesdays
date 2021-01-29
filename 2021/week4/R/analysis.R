25f46c54c918f1fbfc24ace5d6bbfcdc898d513f
Jake Kaupp <jake.kaupp@queensu.ca>
2021-01-28 19:59:30 -0500

    finished week4 2021

--- 2021/week4/R/analysis.R
+++ 2021/week4/R/analysis.R
@@ -20,12 +20,14 @@ plot_data <- summary_data %>%
   filter(name == "grand_total") %>% 
   pack_bars(number_rows = 10, value_column = value, fill_color = "#FE9920")
 
-ggplot(plot_data) +
-  geom_rect(aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = fill), color = "white", size = 0.1) +
+plot <- ggplot(plot_data) +
+  geom_rect(aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = fill, color = colorspace::darken(fill)), size = 0.05) +
   geom_text(data = filter(plot_data, fill == "#FE9920"), aes(x = xmin, y = (ymin + ymax)/2, label = toupper(parent_company)), family = "Rockwell", color = "white", nudge_x = 100, hjust = 0) +
   scale_fill_identity() +
+  scale_color_identity() +
   scale_y_continuous(expand = c(0, 0)) +
   scale_x_continuous(labels = scales::comma,
+                     breaks = seq(0,30000,5000),
                      expand = c(0,0)) +
   labs(x = "Number of Plastic Items Recovered",
        y = NULL,
@@ -41,3 +43,5 @@ ggplot(plot_data) +
            base_family = "Rockwell") +
   theme(axis.text.y = element_blank(),
         plot.title.position = "panel")
+
+ggsave(here("2021", "week4", "tw4_plot.png"), plot, device = ragg::agg_png(), width = 14, height = 7)
